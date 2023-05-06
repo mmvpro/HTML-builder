395ac0491cb87b6f4e6b14aecb2038658b7b82e6
mmvpro <dublibum@gmail.com>
2023-05-06 19:29:03 +0300

    done: 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+console.log('Please enter text: ');
+
+const link = path.join(__dirname, './text.txt');
+
+process.stdout.on('data',(data)=>{
+  let  inputText = data.toString();
+
+  checkInputText(inputText);
+
+  fs.appendFile(link, inputText, (err)=>{
+    if(err) throw err;
+  });
+  
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+process.on('exit',()=>{
+  console.log('\nEnding of the process, Bye!');
+});
+
+function checkInputText(inputText){
+  if(inputText === 'exit\n'){
+    process.exit();
+  }
+}
