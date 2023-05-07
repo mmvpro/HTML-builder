fdefc568485a7ae22911700550ffdffd4de35924
mmvpro <dublibum@gmail.com>
2023-05-07 15:41:37 +0300

    done: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,56 @@
+const { access } = require('node:fs');
+const { mkdir, readdir, copyFile,  unlink } = require('node:fs/promises');
+const { join } = require('node:path');
 
+makeDirectory().catch(console.error);
+getAllFilesAndCopy();
+cleanDir();
+
+async function makeDirectory() {
+  const fileCopy = join(__dirname, 'files-Copy');
+  const dirCreation = await mkdir(fileCopy, { recursive: true });
+  return dirCreation;
+}
+
+async function copyF( source, destination){
+  try {
+    await copyFile(source, destination);
+
+  } catch {
+    console.error('The file could not be copied');
+  }
+} 
+
+async function getAllFilesAndCopy(){
+  try {
+    const filesFolder = await readdir(join(__dirname, 'files'));
+        
+    for(const file of filesFolder){
+      const source = join(__dirname,'files', file);
+      const destination = join(__dirname, 'files-Copy',file);
+            
+      copyF(source,destination);
+    }
+  } catch (error) {
+    console.error(error);
+  }    
+}
+
+async function cleanDir(){
+  try {
+    const filesCopy = await readdir(join(__dirname,'files-Copy'));
+    
+    for(const file of filesCopy){
+      access(join(__dirname,'files', file),(err)=>{
+        
+        if(err){
+          unlink(join(__dirname,'files-Copy',file), (error)=>{
+            if(error) console.error(error);
+          });
+        }
+      }); 
+    }
+  } catch (error) {
+    console.error(error);
+  }  
+}
